2ec72cef1f327cb6054d1e33a2806d8a7874dc6f
marcus aurelio <marcusaurelio743@gmail.com>
2024-11-16 09:50:25 -0300

    criando end-point de produtos e testando metodo de salvar

---
+++ vendas-api/src/main/java/vendasapi/rest/produtos/ProdutoController.java
@@ -0,0 +1,18 @@
+package vendasapi.rest.produtos;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+	
+	@PostMapping
+	public ProdutoFormRequest salvar(@RequestBody ProdutoFormRequest produto) {
+		System.out.println(produto);
+		return produto;
+	}
+
+}
