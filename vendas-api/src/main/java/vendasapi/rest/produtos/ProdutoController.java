16ddaeb69d5f1d14d9213ad3c23117e621bd396f
marcus aurelio <marcusaurelio743@gmail.com>
2024-11-17 22:59:23 -0300

    criando end-pont de atualizar produtos

--- vendas-api/src/main/java/vendasapi/rest/produtos/ProdutoController.java
+++ vendas-api/src/main/java/vendasapi/rest/produtos/ProdutoController.java
@@ -1,8 +1,13 @@
 package vendasapi.rest.produtos;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +32,22 @@ public class ProdutoController {
 		
 		
 		return ProdutoFormRequest.fromModel(EntidadeProduto);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<?> Atualizar(@PathVariable Long id, @RequestBody ProdutoFormRequest produto) {
+		Optional<Produto> produtoExistente = produtoRepository.findById(id);
+		
+		if(produtoExistente.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		Produto entidadeProduto = produto.toModel();
+		entidadeProduto.setId(id);
+		entidadeProduto.setDataCadastro(produtoExistente.get().getDataCadastro());
+		produtoRepository.save(entidadeProduto);
+		
+		return ResponseEntity.ok().build();
 	}
 
 }
